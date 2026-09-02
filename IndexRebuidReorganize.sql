17dab42d893098af83ba030cb92fa4842b9544a8
Jibin Benny <114920325+JibinEBenny@users.noreply.github.com>
2026-09-02 11:02:35 +0530

    Update IndexRebuidReorganize.sql

--- IndexRebuidReorganize.sql
+++ IndexRebuidReorganize.sql
@@ -3,7 +3,7 @@
 ---------------------------------------------------------------------------------------------------------------------------------------------------------
 -- Run with reorganize only on DEV & TEST environments because it will take time according to the size 
 
-ALTER INDEX IDX_ERROR_DETAIL_ErrorGUID ON dbo.ERROR_DETAIL REORGANIZE;
+ALTER INDEX IDX_INDEXNAME ON dbo.TABLE_NAME REORGANIZE;
 
 -- Use REORGANIZE for moderate fragmentation (≈5–30%) when minimal blocking is required.
 -- Reorganize is always an online operation. This means long-term object-level locks aren't held and queries or updates to the underlying table
@@ -11,7 +11,7 @@ ALTER INDEX IDX_ERROR_DETAIL_ErrorGUID ON dbo.ERROR_DETAIL REORGANIZE;
 ---------------------------------------------------------------------------------------------------------------------------------------------------------
 -- REBUID
 ---------------------------------------------------------------------------------------------------------------------------------------------------------
-ALTER INDEX IX_Employee_LastName ON HumanResources.Employee REBUILD;
+ALTER INDEX IX_IDX_INDEXNAME ON dbo.TABLE_NAME REBUILD;
 
 -- Use REBUILD for heavy fragmentation (>30%) or when you need updated statistics and faster completion on large indexes.
 -- It can be online or offline . If online it will not affect the underlaying table else it will affect the table it will not be able to read or write
